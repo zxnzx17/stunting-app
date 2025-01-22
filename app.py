d24f4b2c9800ee730b9db55a9d4057bed56c82e8
Afdol Fahmul Rozi <afdolfarosir23@gmail.com>
2025-01-23 00:39:30 +0700

    nothing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from streamlit_option_menu import option_menu
 
 # Konfigurasi judul utama aplikasi
 st.set_page_config(page_title="Aplikasi Stunting", layout="wide")
